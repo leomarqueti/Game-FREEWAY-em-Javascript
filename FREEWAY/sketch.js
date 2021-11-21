75c01642942025e2494c4ee840605f8b3e678b49
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2021-11-21 20:16:05 -0300

    Add files via upload

---
+++ FREEWAY/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 400);
+  somTransito.loop();
+}
+
+function draw() {
+  background(imagemFundoJogo);
+  mostrarAtor();
+  mostrarCarro();
+  movimentaCarro();
+  controlaAtor();
+  voltaCarroPosicaoInicial();
+  verificaColisao();
+  mostraPontos();
+  calculaPontos();
+  dificuldade();
+}
